ef5f86c6838388987cfbba5d81a04bb5e23e1911
Lupoiu Alin <alin.lupoiu@freshbyteinc.com>
2019-11-08 13:33:37 +0200

    horoku

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const viewPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
 const app = express();
+const port = process.env.PORT || 3000
 //setup hendelbars engine and view location
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
@@ -102,6 +103,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server si up on port 3000');
+app.listen(port, () => {
+    console.log('Server si up on port '+port);
 })
